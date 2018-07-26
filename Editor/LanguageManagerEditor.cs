af0fcd3cf15ab0aea48d86d9f34bfe2b320592a5
James Liu <contact@jamessliu.com>
2018-07-26 04:25:08 -0700

    Update to new version

--- Editor/LanguageManagerEditor.cs
+++ Editor/LanguageManagerEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEditor;
@@ -6,37 +7,39 @@ using UnityEngine;
 
 namespace HouraiTeahouse.Localization {
 
-    /// <summary> A custom Editor for LanguageManager. </summary>
-    [CustomEditor(typeof(LanguageManager))]
-    internal class LanguageManagerEditor : Editor {
+/// <summary> A custom Editor for LanguageManager. </summary>
+[CustomEditor(typeof(LanguageManager))]
+internal class LanguageManagerEditor : Editor {
 
-        string[] _availableLanguages;
-        string[] _display;
-        int _index;
+  string[] _availableLanguages;
+  string[] _display;
+  int _index;
 
-        /// <summary>
-        ///     <see cref="UnityEditor.Editor.OnInspectorGUI" />
-        /// </summary>
-        public override void OnInspectorGUI() {
-            base.OnInspectorGUI();
-            var langManager = target as LanguageManager;
+  /// <summary>
+  ///     <see cref="UnityEditor.Editor.OnInspectorGUI" />
+  /// </summary>
+  public override void OnInspectorGUI() {
+    base.OnInspectorGUI();
+    var langManager = target as LanguageManager;
 
-            if (_availableLanguages == null) {
-                _availableLanguages = langManager.AvailableLanguages.ToArray();
-                _display = _availableLanguages.Select(Language.GetName).ToArray();
-                Language language = langManager.CurrentLanguage;
-                if (language != null)
-                    _index = Array.LastIndexOf(_availableLanguages, langManager.CurrentLanguage.Name);
-            }
-
-            GUI.enabled = EditorApplication.isPlayingOrWillChangePlaymode;
-            int oldIndex = _index;
-            _index = EditorGUILayout.Popup("Current Language", _index, _display);
-            if (_index != oldIndex)
-                langManager.LoadLanguage(_availableLanguages[_index]);
-            GUI.enabled = true;
-        }
-
+    if (_availableLanguages == null) {
+      _availableLanguages = langManager.AvailableLanguages.ToArray();
+      _display = _availableLanguages.Select(GetLanguageName).ToArray();
+      Language language = langManager.CurrentLanguage;
+      if (language != null)
+          _index = Array.LastIndexOf(_availableLanguages, langManager.CurrentLanguage.Name);
     }
+
+    GUI.enabled = EditorApplication.isPlayingOrWillChangePlaymode;
+    int oldIndex = _index;
+    _index = EditorGUILayout.Popup("Current Language", _index, _display);
+    if (_index != oldIndex)
+        langManager.LoadLanguage(_availableLanguages[_index]);
+    GUI.enabled = true;
+  }
+
+  string GetLanguageName(string name) => CultureInfo.GetCultureInfo(name).DisplayName;
+
+}
 
 }
--- Runtime/LanguageManager.cs
+++ Runtime/LanguageManager.cs
@@ -1,200 +1,150 @@
+using HouraiTeahouse.Options;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Globalization;
 using System.Linq;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace HouraiTeahouse.Localization {
 
-    public class LanguageChanged {
-        public LanguageManager Manager;
-        public Language Language;
-    }
+public class LanguageChangedEvent : UnityEvent<Language> {}
 
-    public interface ILanguageStorage {
-        event Action<string> OnChangeLangauge;
+/// <summary> 
+/// Singleton MonoBehaviour that manages all of localization system. 
+/// </summary>
+public sealed class LanguageManager : MonoBehaviour {
 
-        string GetStoredLangaugeId(string defaultLangauge);
-        void SetStoredLanguageId(string language);
-    }
+  public const string FileExtension = ".json";
 
-    public class PlayerPrefLanguageStorageDelegate : ILanguageStorage {
+  string _storageDirectory;
+  HashSet<string> _languages;
 
-        readonly string _playerPrefKey;
+  public Option LanguageOption;
+  public LanguageChangedEvent OnChangeLangauge;
 
-        public event Action<string> OnChangeLangauge;
+  public static LanguageManager Instance { get; private set; }
 
-        public PlayerPrefLanguageStorageDelegate(string key) {
-            _playerPrefKey = Argument.NotNull(key);
-        }
+  [SerializeField]
+  [Tooltip("The default language to use if the Player's current language is not supported")]
+  string _defaultLanguage = "en";
 
-        public string GetStoredLangaugeId(string defaultLangauge) {
-            if (!PlayerPrefs.HasKey(_playerPrefKey)) 
-                SetStoredLanguageId(defaultLangauge);
-            return PlayerPrefs.GetString(_playerPrefKey);
-        }
+  [SerializeField]
+  [Tooltip("Destroy this object on scene changes?")]
+  bool _dontDestroyOnLoad = false;
 
-        public void SetStoredLanguageId(string language) {
-            if (PlayerPrefs.HasKey(_playerPrefKey) || PlayerPrefs.GetString(_playerPrefKey) != language) {
-                PlayerPrefs.SetString(_playerPrefKey, language);
-                if (OnChangeLangauge != null)
-                    OnChangeLangauge(language);
-            }
-        }
+  [SerializeField]
+  [Tooltip("The Resources directory to load the Language files from")]
+  string localizationDirectory = "lang";
 
-    }
+  /// <summary> 
+  /// The currently used language. 
+  /// </summary>
+  public Language CurrentLanguage { get; private set; }
 
-    /// <summary> 
-    /// Singleton MonoBehaviour that manages all of localization system. 
+  /// <summary> 
+  /// All available languages currently supported by the system. 
+  /// </summary>
+  public IEnumerable<string> AvailableLanguages => _languages ?? Enumerable.Empty<string>();
+
+  /// <summary> 
+  /// Gets an enumeration of all of the localizable keys. 
+  /// </summary>
+  public IEnumerable<string> Keys => CurrentLanguage.Keys;
+
+  void SetLanguage(string langName, IDictionary<string, string> values) {
+    if (CurrentLanguage.Name == langName) return;
+    CurrentLanguage.Update(values);
+    CurrentLanguage.Name = langName;
+    OnChangeLangauge?.Invoke(CurrentLanguage);
+    var culture = CultureInfo.GetCultureInfo(langName);
+    Debug.LogFormat($"Set language to {culture.DisplayName}");
+  }
+
+  string GetLanguagePath(string identifier) {
+      return Path.Combine(_storageDirectory, identifier + FileExtension);
+  }
+
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
     /// </summary>
-    public sealed class LanguageManager : MonoBehaviour {
+    void Awake() {
+      Instance = this;
 
-        public const string FileExtension = ".json";
-        public static event Action<Language> OnChangeLangauge;
-
-        string _storageDirectory;
-        HashSet<string> _languages;
-
-        static ILanguageStorage _storageDelegate;
-
-        public static LanguageManager Instance { get; private set; }
-
-        static LanguageManager() {
-            Storage = new PlayerPrefLanguageStorageDelegate("lang");
-        }
-
-        static void StorageChangeLangauge(string language) {
-            if (Instance == null)
-                return;
-            Instance.LoadLanguage(language);
-        }
-
-        public static ILanguageStorage Storage {
-            get { return _storageDelegate; }
-            set { 
-                Argument.NotNull(value);
-                if (_storageDelegate != null)
-                    _storageDelegate.OnChangeLangauge -= StorageChangeLangauge;
-                _storageDelegate = value;
-                if (_storageDelegate != null)
-                    _storageDelegate.OnChangeLangauge += StorageChangeLangauge;
-            }
-        }
-
-        [SerializeField]
-        [Tooltip("The default language to use if the Player's current language is not supported")]
-        string _defaultLanguage = "en";
-
-        [SerializeField]
-        [Tooltip("Destroy this object on scene changes?")]
-        bool _dontDestroyOnLoad = false;
-
-        [SerializeField]
-        [Tooltip("The Resources directory to load the Language files from")]
-        string localizationDirectory = "lang";
-
-        /// <summary> 
-        /// The currently used language. 
-        /// </summary>
-        public Language CurrentLanguage { get; private set; }
-
-        /// <summary> 
-        /// All available languages currently supported by the system. 
-        /// </summary>
-        public IEnumerable<string> AvailableLanguages {
-            get { return _languages ?? Enumerable.Empty<string>();}
-        }
-
-        /// <summary> 
-        /// Gets an enumeration of all of the localizable keys. 
-        /// </summary>
-        public IEnumerable<string> Keys {
-            get { return CurrentLanguage.Keys;}
-        }
-
-        void SetLanguage(string langName, IDictionary<string, string> values) {
-            if (CurrentLanguage.Name == langName)
-                return;
-            CurrentLanguage.Update(values);
-            CurrentLanguage.Name = langName;
-            OnChangeLangauge?.Invoke(CurrentLanguage);
-            Debug.LogFormat($"Set language to {Language.GetName(langName)}");
-        }
-
-        string GetLanguagePath(string identifier) {
-            return Path.Combine(_storageDirectory, identifier + FileExtension);
-        }
-
-        /// <summary>
-        /// Awake is called when the script instance is being loaded.
-        /// </summary>
-        void Awake() {
-            Instance = this;
-
-            CurrentLanguage = new Language();
+      CurrentLanguage = new Language();
 #if HOURAI_EVENTS
-            _eventManager = Mediator.Global;
+      _eventManager = Mediator.Global;
 #endif
 
-            _storageDirectory = Path.Combine(Application.streamingAssetsPath, localizationDirectory);
-            var languages = Directory.GetFiles(_storageDirectory);
-            _languages = new HashSet<string>(from file in languages
-                                             where file.EndsWith(FileExtension)
-                                             select Path.GetFileNameWithoutExtension(file));
+      _storageDirectory = Path.Combine(Application.streamingAssetsPath, localizationDirectory);
+      var languages = Directory.GetFiles(_storageDirectory);
+      _languages = new HashSet<string>(from file in languages
+                                      where file.EndsWith(FileExtension)
+                                      select Path.GetFileNameWithoutExtension(file));
 
-            SystemLanguage systemLang = Application.systemLanguage;
-            string currentLang = Storage.GetStoredLangaugeId(systemLang.ToIdentifier());
-            if (!_languages.Contains(currentLang) || systemLang == SystemLanguage.Unknown) {
-                Debug.LogFormat("No language data for \"{0}\" found. Loading default language: {1}", _defaultLanguage, currentLang);
-                currentLang = _defaultLanguage;
-            }
-            LoadLanguage(currentLang);
-            Storage.SetStoredLanguageId(currentLang);
+      SetupLanguageOption();
 
-            if (_dontDestroyOnLoad)
-                DontDestroyOnLoad(this);
-        }
+      string currentLang = GetStoredLangaugeId();
+      if (!_languages.Contains(currentLang)) {
+          Debug.LogFormat("No language data for \"{0}\" found. Loading default language: {1}", _defaultLanguage, currentLang);
+          currentLang = _defaultLanguage;
+      }
+      LoadLanguage(currentLang);
+      SetStoredLanguageId(currentLang);
 
-        /// <summary>
-        /// This function is called when the MonoBehaviour will be destroyed.
-        /// </summary>
-        void OnDestroy() {
-            Save();
-        }
+      if (_dontDestroyOnLoad)
+          DontDestroyOnLoad(this);
+  }
 
-        /// <summary>
-        /// Callback sent to all game objects before the application is quit.
-        /// </summary>
-        void OnApplicationQuit() {
-            Save();
-        }
+  void SetupLanguageOption() {
+    CultureInfo systemLang = Application.systemLanguage.ToCultureInfo();
 
-        /// <summary> 
-        /// Saves the current language preferences to PlayerPrefs to keep it persistent. 
-        /// </summary>
-        void Save() {
-            Storage.SetStoredLanguageId(CurrentLanguage.Name);
-        }
+    LanguageOption.Type = OptionType.Enum;
+    LanguageOption.MinValue = int.MinValue;
+    LanguageOption.MaxValue = int.MaxValue;
+    LanguageOption.DefaultValue = systemLang.LCID;
+    LanguageOption.EnumOptions = _languages.Select(lang => {
+      var culture = CultureInfo.GetCultureInfo(lang);
+      return new EnumOption { Value = culture.LCID, DisplayName = culture.DisplayName };
+    }).OrderBy(opt => opt.DisplayName)
+    .ToArray();
 
-        /// <summary> 
-        /// Loads a new language given the Microsoft language identifier. 
-        /// </summary>
-        /// <param name="identifier"> the Microsoft identifier for a lanuguage </param>
-        /// <returns> the localization language </returns>
-        /// <exception cref="ArgumentNullException"> <paramref name="identifier" /> is null. </exception>
-        /// <exception cref="InvalidOperationException"> the language specified by <paramref name="identifier" /> is not currently
-        /// supported. </exception>
-        public Language LoadLanguage(string identifier) {
-            Argument.NotNull(identifier);
-            identifier = identifier.ToLower();
-            if (!_languages.Contains(identifier))
-                throw new InvalidOperationException(string.Format("Language with identifier of {0} is not supported.", identifier));
-            var languageValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(GetLanguagePath(identifier)));
-            SetLanguage(identifier, languageValues);
-            return CurrentLanguage;
-        }
+    LanguageOption.OnValueChanged.AddListener(() => {
+      LoadLanguage(GetStoredLangaugeId());
+    });
+  }
 
-    }
+  string GetStoredLangaugeId() {
+    int lcid = LanguageOption.Get<int>();
+    return CultureInfo.GetCultureInfo(lcid).Name;
+  }
+
+  void SetStoredLanguageId(string languageId) {
+    var culture = CultureInfo.GetCultureInfo(languageId);
+    LanguageOption.Set<int>(culture.LCID);
+  }
+
+  /// <summary> 
+  /// Loads a new language given the Microsoft language identifier. 
+  /// </summary>
+  /// <param name="identifier"> the Microsoft identifier for a lanuguage </param>
+  /// <returns> the localization language </returns>
+  /// <exception cref="ArgumentNullException"> <paramref name="identifier" /> is null. </exception>
+  /// <exception cref="InvalidOperationException"> the language specified by <paramref name="identifier" /> is not currently
+  /// supported. </exception>
+  public Language LoadLanguage(string identifier) {
+      Argument.NotNull(identifier);
+      identifier = identifier.ToLower();
+      if (!_languages.Contains(identifier))
+          throw new InvalidOperationException(string.Format("Language with identifier of {0} is not supported.", identifier));
+      var languageValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(GetLanguagePath(identifier)));
+      SetLanguage(identifier, languageValues);
+      return CurrentLanguage;
+  }
+
+}
 
 }
